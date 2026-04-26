bf301dc619b6d71e084e1c91e9e0fb397e2cf1d5
KosFal <fkp5025@gmail.com>
2026-04-26 14:27:35 +0300

    Refactor asyncMap to support promises and abort signals

--- async-array.js
+++ async-array.js
@@ -1,86 +1,79 @@
-function asyncMap(array, asyncTask, cb, signal) {
-  var results = [];
-  var completed = 0;
-  var timers = [];      
-  var aborted = false;
+function tripleAsync(num, timers, cb) {
+  var t = setTimeout(function() {
+    cb(null, num * 3);
+  }, 200);
+  timers.push(t);
+}
 
-  function onAbort() {
-    aborted = true;
-    timers.forEach(function(t) { clearTimeout(t); });
-    timers = [];
-    cb(new Error("AbortError: операцію скасовано"), null);
-  }
-
-  if (signal) {
-    if (signal.aborted) {
-      return cb(new Error("AbortError: вже скасовано"), null);
-    }
-    signal.addEventListener("abort", onAbort);
-  }
-
-  if (array.length === 0) {
-    if (signal) signal.removeEventListener("abort", onAbort);
-    return cb(null, []);
-  }
-
-  array.forEach(function(item, index) {
-    asyncTask(item, timers, function(err, value) {
-      if (aborted) return; 
-
-      if (err) {
-        if (signal) signal.removeEventListener("abort", onAbort);
-        return cb(err, null);
-      }
-
-      results[index] = value;
-      completed = completed + 1;
-
-      if (completed === array.length) {
-        if (signal) signal.removeEventListener("abort", onAbort); 
-        cb(null, results);
-      }
-    });
+function triplePromise(num, timers) {
+  return new Promise(function(resolve) {
+    var t = setTimeout(function() {
+      resolve(num * 3);
+    }, 200);
+    timers.push(t);
   });
 }
 
-function asyncMapPromise(array, asyncTask, signal) {
-  return new Promise(function(resolve, reject) {
-    var results = [];
-    var completed = 0;
-    var timers = [];
+console.log("=== demo 1: callback ===");
+asyncMap([1, 2, 3, 4], tripleAsync, function(err, result) {
+  if (err) return console.error("помилка:", err.message);
+  console.log("callback result:", result); 
+});
 
-    function onAbort() {
-      timers.forEach(function(t) { clearTimeout(t); }); 
-      timers = [];
-      reject(new Error("AbortError: операцію скасовано"));
-    }
-
-    if (signal) {
-      if (signal.aborted) {
-        return reject(new Error("AbortError: вже скасовано"));
-      }
-      signal.addEventListener("abort", onAbort);
-    }
-
-    if (array.length === 0) {
-      if (signal) signal.removeEventListener("abort", onAbort);
-      return resolve([]);
-    }
-
-    array.forEach(function(item, index) {
-      asyncTask(item, timers)
-        .then(function(value) {
-          results[index] = value;
-          completed = completed + 1;
-          if (completed === array.length) {
-            if (signal) signal.removeEventListener("abort", onAbort); 
-            resolve(results);
-          }
-        })
-        .catch(function(err) {
-          if (signal) signal.removeEventListener("abort", onAbort);
-          reject(err);
-        });
-    });
+console.log("=== demo 2: promise ===");
+asyncMapPromise([5, 10, 15], triplePromise)
+  .then(function(result) {
+    console.log("promise result:", result); 
+  })
+  .catch(function(err) {
+    console.error("помилка:", err.message);
   });
+
+console.log("=== demo 3: async/await ===");
+async function runAwait() {
+  try {
+    var result = await asyncMapPromise([2, 4, 6], triplePromise);
+    console.log("await result:", result); 
+  } catch (err) {
+    console.error("помилка:", err.message);
+  }
 }
+runAwait();
+
+console.log("=== demo 4: callback + abort ===");
+var controller1 = new AbortController();
+
+asyncMap([10, 20, 30], tripleAsync, function(err, result) {
+  if (err) {
+    console.log("callback скасовано:", err.message); 
+  } else {
+    console.log("callback result (не мало виконатись):", result);
+  }
+}, controller1.signal);
+
+setTimeout(function() {
+  controller1.abort(); 
+}, 50);
+
+console.log("=== demo 5: promise + abort ===");
+var controller2 = new AbortController();
+
+asyncMapPromise([7, 8, 9], triplePromise, controller2.signal)
+  .then(function(result) {
+    console.log("promise result (не мало виконатись):", result);
+  })
+  .catch(function(err) {
+    console.log("promise скасовано:", err.message); 
+  });
+
+setTimeout(function() {
+  controller2.abort();
+}, 50);
+
+console.log("=== demo 6: abort до старту ===");
+var controller3 = new AbortController();
+controller3.abort(); 
+
+asyncMap([1, 2], tripleAsync, function(err, result) {
+  console.log("вже скасовано одразу:", err.message); 
+}, controller3.signal);
