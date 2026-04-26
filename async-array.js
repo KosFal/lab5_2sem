1cc9ee2bc2f23dd8c999b52a0b8df48f4d33cb45
KosFal <fkp5025@gmail.com>
2026-04-26 11:50:57 +0300

    Enhance asyncMap and asyncMapPromise with abort support
    Added support for aborting async operations using an AbortSignal. Updated asyncMap and asyncMapPromise functions to handle abort events and clear timers appropriately.

--- async-array.js
+++ async-array.js
@@ -1,65 +1,86 @@
-function asyncMap(array, asyncTask, cb) {
+function asyncMap(array, asyncTask, cb, signal) {
   var results = [];
   var completed = 0;
+  var timers = [];      
+  var aborted = false;
+
+  function onAbort() {
+    aborted = true;
+    timers.forEach(function(t) { clearTimeout(t); });
+    timers = [];
+    cb(new Error("AbortError: операцію скасовано"), null);
+  }
+
+  if (signal) {
+    if (signal.aborted) {
+      return cb(new Error("AbortError: вже скасовано"), null);
+    }
+    signal.addEventListener("abort", onAbort);
+  }
 
   if (array.length === 0) {
+    if (signal) signal.removeEventListener("abort", onAbort);
     return cb(null, []);
   }
 
   array.forEach(function(item, index) {
-    asyncTask(item, function(err, value) {
+    asyncTask(item, timers, function(err, value) {
+      if (aborted) return; 
+
       if (err) {
+        if (signal) signal.removeEventListener("abort", onAbort);
         return cb(err, null);
       }
+
       results[index] = value;
       completed = completed + 1;
+
       if (completed === array.length) {
+        if (signal) signal.removeEventListener("abort", onAbort); 
         cb(null, results);
       }
     });
   });
 }
 
-function asyncMapPromise(array, asyncTask) {
+function asyncMapPromise(array, asyncTask, signal) {
   return new Promise(function(resolve, reject) {
     var results = [];
     var completed = 0;
+    var timers = [];
+
+    function onAbort() {
+      timers.forEach(function(t) { clearTimeout(t); }); 
+      timers = [];
+      reject(new Error("AbortError: операцію скасовано"));
+    }
+
+    if (signal) {
+      if (signal.aborted) {
+        return reject(new Error("AbortError: вже скасовано"));
+      }
+      signal.addEventListener("abort", onAbort);
+    }
 
     if (array.length === 0) {
+      if (signal) signal.removeEventListener("abort", onAbort);
       return resolve([]);
     }
 
     array.forEach(function(item, index) {
-      asyncTask(item)
+      asyncTask(item, timers)
         .then(function(value) {
           results[index] = value;
           completed = completed + 1;
           if (completed === array.length) {
+            if (signal) signal.removeEventListener("abort", onAbort); 
             resolve(results);
           }
         })
         .catch(function(err) {
+          if (signal) signal.removeEventListener("abort", onAbort);
           reject(err);
         });
     });
   });
 }
-
-function doublePromise(num) {
-  return new Promise(function(resolve) {
-    setTimeout(function() {
-      resolve(num * 2);
-    }, 100);
-  });
-}
-
-async function runExample() {
-  try {
-    var result = await asyncMapPromise([1, 2, 3], doublePromise);
-    console.log(result); 
-  } catch (err) {
-    console.error("помилка:", err);
-  }
-}
-
-runExample();
